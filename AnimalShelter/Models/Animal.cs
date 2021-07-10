45fcf2cfa70042325c6e4641645175d30484c985
Garrett Brown <garrettpaulbrown@gmail.com>
2021-07-09 22:02:25 -0700

    Add validations.

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelter.Models
 {
     public class Animal
     {
         public int AnimalId {get; set; }
+        [Required]
+        [StringLength(20)]
         public string Name {get; set; }
+        [Required]
         public string Species {get; set; }
+        [Required]
+        [Range(1,200, ErrorMessage = "Age must be between 1 and 100.")]
         public int Age {get; set; }
+        [Required]
         public string Sex {get; set; }
     }
 }
