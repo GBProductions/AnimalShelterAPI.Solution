15f041c603bfa0f8e6ec3cb33b050854d83c10b1
Garrett Brown <garrettpaulbrown@gmail.com>
2021-07-09 21:30:19 -0700

    Seed database, do migration.

---
+++ AnimalShelter/Migrations/20210710042613_SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Name", "Sex", "Species" },
+                values: new object[,]
+                {
+                    { 1, 9, "Amy", "Female", "Dog" },
+                    { 2, 1, "Jim", "Male", "Cat" },
+                    { 3, 5, "Pam", "Female", "Dog" },
+                    { 4, 3, "Alice", "Female", "Cat" },
+                    { 5, 8, "Dee", "Female", "Dog" },
+                    { 6, 9, "Jimmy", "Male", "Cat" },
+                    { 7, 6, "Edward", "Male", "Cat" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 7);
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -4,6 +4,19 @@ namespace AnimalShelter.Models
 {
     public class AnimalShelterContext : DbContext
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Animal>()
+                .HasData(
+                    new Animal { AnimalId = 1, Name = "Amy", Species = "Dog", Age = 9, Sex = "Female" },
+                    new Animal { AnimalId = 2, Name = "Jim", Species = "Cat", Age = 1, Sex = "Male" },
+                    new Animal { AnimalId = 3, Name = "Pam", Species = "Dog", Age = 5, Sex = "Female" },
+                    new Animal { AnimalId = 4, Name = "Alice", Species = "Cat", Age = 3, Sex = "Female" },
+                    new Animal { AnimalId = 5, Name = "Dee", Species = "Dog", Age = 8, Sex = "Female" },
+                    new Animal { AnimalId = 6, Name = "Jimmy", Species = "Cat", Age = 9, Sex = "Male" },
+                    new Animal { AnimalId = 7, Name = "Edward", Species = "Cat", Age = 6, Sex = "Male" }
+                );
+        }
         public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
             : base(options)
         {
